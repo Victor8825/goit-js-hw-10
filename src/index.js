b95b36f6d359fda0aff481c9147368df7c9ace65
Victor8825 <victorrv2588@gmail.com>
2022-06-23 00:00:54 +0300

    one card markup done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
-var debounce = require('lodash.debounce');
-// import {debounce} from "../node_modules/lodash.debounce/index.js";
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 const searchParams = new URLSearchParams ({
@@ -9,20 +8,46 @@ fields: [
 });
 
 const inputRef = document.querySelector("#search-box");
+const countryCardWrap = document.querySelector(".country-info");
 
 inputRef.addEventListener("input", debounce( () => {
   fetchCountries(inputRef.value)
-  .then(country => {
-    console.log(country);
-  buildCountryCardMarkup(country)
-})  
+    .then(country => {
+      console.log(buildCardMarkup(country[0]));
+      countryCardWrap.innerHTML = buildCardMarkup(country[0]); 
+    })  
 }, DEBOUNCE_DELAY));
 
 function fetchCountries(name) {
   return fetch(`https://restcountries.com/v3.1/name/${name}?${searchParams}`)
-  .then(response => response.json())
+    .then(response => response.json())
 }
 
-function buildCountryCardMarkup(country) {
-
+function buildCardMarkup({flags, name, capital, population, languages}) {
+  let languagesArray = [];
+  const values = Object.values(languages).join(", ");
+  for(const value of values) {
+    languagesArray += value;
+  }
+  return `<h2>
+  <img src="${flags.svg}" alt="country flag" width="16" height="16"/>${name.official}</h2>
+  <ul class="country-info">
+  <li>Capital: ${capital}</li>
+  <li>Population: ${population}</li>
+  <li>Languages: ${languagesArray}</li>
+  </ul>`;
 }
+  
+
+
+
+
+
+  // else (inputRef.value === "") {
+  //   inputRef.value.trim();
+  // }
+
+
+// function cleanInput () {
+
+// }
