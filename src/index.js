4e0ea6054875570e2ab5323ee3e23cf1b616e2d1
Victor8825 <victorrv2588@gmail.com>
2022-06-23 14:00:43 +0300

    just optimised left

--- src/index.js
+++ src/index.js
@@ -1,26 +1,53 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
 const debounce = require('lodash.debounce');
-
 const DEBOUNCE_DELAY = 300;
-const searchParams = new URLSearchParams ({
-fields: [
-  "name", "capital", "population", "flags", "languages"]
-});
-
 const inputRef = document.querySelector("#search-box");
-const countryCardWrap = document.querySelector(".country-info");
+const countryCardWrapRef = document.querySelector(".country-info");
+const countryListRef = document.querySelector(".country-list");
+const searchParams = new URLSearchParams ({
+  fields: [
+    "name", "capital", "population", "flags", "languages"]
+  });
 
 inputRef.addEventListener("input", debounce( () => {
+  if (inputRef.value === "") {
+    cleanInput();
+    return;
+  }
   fetchCountries(inputRef.value)
     .then(country => {
-      console.log(buildCardMarkup(country[0]));
-      countryCardWrap.innerHTML = buildCardMarkup(country[0]); 
-    })  
+      if (country.length > 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.");
+      }
+      if (country.length >= 2 && country.length <= 10) {
+        countryCardWrapRef.innerHTML = "";
+        countryListRef.innerHTML = buildCardListMarkup(country); 
+      }
+      if (country.length === 1) {
+        countryListRef.innerHTML = "";
+        countryCardWrapRef.innerHTML = buildCardMarkup(country[0]); 
+      }
+      if (inputRef.value === "") {
+        cleanInput();
+      }      
+    })
+    .catch(() => {
+      countryCardWrapRef.innerHTML = "";
+      countryListRef.innerHTML = "";
+    }); 
 }, DEBOUNCE_DELAY));
 
 function fetchCountries(name) {
   return fetch(`https://restcountries.com/v3.1/name/${name}?${searchParams}`)
-    .then(response => response.json())
+    .then(response =>{
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
 }
 
 function buildCardMarkup({flags, name, capital, population, languages}) {
@@ -37,17 +64,16 @@ function buildCardMarkup({flags, name, capital, population, languages}) {
   <li>Languages: ${languagesArray}</li>
   </ul>`;
 }
+
+function buildCardListMarkup (country) {
+  return country.map( countr => 
+   `<li><img src="${countr.flags.svg}" alt="country flag" width="16" height="16"/>${countr.name.official}</li>`
+  );
+}
+ 
+function cleanInput () {
+    inputRef.value.trim();
+    countryCardWrapRef.innerHTML = "";
+    countryListRef.innerHTML = "";
+}
   
-
-
-
-
-
-  // else (inputRef.value === "") {
-  //   inputRef.value.trim();
-  // }
-
-
-// function cleanInput () {
-
-// }
